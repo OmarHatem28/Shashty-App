44f4cd50608e86a63f25a6ea045801970b02c8be
Omar Hatem <omarh.ismail1@gmail.com>
2019-08-27 12:11:30 +0200

    Fetch API Data
    * Implement AppBar UI

--- lib/Views/home.dart
+++ lib/Views/home.dart
@@ -1,18 +1,42 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: buildAppBar(),
-      body: Container(),
+    return FutureBuilder(
+      future: fetchData(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) return CircularProgressIndicator();
+        return Scaffold(
+          appBar: buildAppBar(snapshot.data['categories']),
+          body: Container(),
+        );
+      },
     );
   }
 
-  Widget buildAppBar() {
+  Widget buildAppBar(List categories) {
+    categories.insert(0, {'name': 'الرئيسية'});
     return AppBar(
       title: Icon(Icons.live_tv),
       centerTitle: true,
+      bottom: TabBar(
+        tabs: categories.map((category) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(category['name']),
+          );
+        }).toList(),
+        controller: TabController(length: categories.length, vsync: AnimatedListState()),
+        isScrollable: true,
+        indicatorColor: Colors.red,
+        onTap: (selected) {
+          print(selected);
+        },
+      ),
       actions: <Widget>[
         Padding(
           padding: const EdgeInsets.all(8.0),
@@ -20,5 +44,14 @@ class Home extends StatelessWidget {
         ),
       ],
     );
+  }
+
+  Future<Map> fetchData() async {
+    var url = 'http://shashtyapi.sports-mate.net/api/Home';
+
+    http.Response response = await http.get(url);
+
+    print(json.decode(response.body)['categories']);
+    return json.decode(response.body);
   }
 }
