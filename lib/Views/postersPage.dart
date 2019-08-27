74b92c6ce9d7ef1af412a504688d8ca7b7ab240c
Omar Hatem <omarh.ismail1@gmail.com>
2019-08-27 18:31:53 +0200

    Add PostersPage UI

--- lib/Views/postersPage.dart
+++ lib/Views/postersPage.dart
@@ -3,6 +3,18 @@ import 'package:flutter/material.dart';
 class PostersPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return GridView.builder(
+        gridDelegate:
+            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3, childAspectRatio: 0.7),
+        itemCount: 4,
+        itemBuilder: (context, i) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ClipRRect(
+              child: Image.asset('assets/images/img.jpg', fit: BoxFit.cover,),
+              borderRadius: BorderRadius.circular(15),
+            ),
+          );
+        });
   }
 }
